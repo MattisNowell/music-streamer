4508cea7c06a2ba93e292447d59e75ae47d927a0
NowellM <mattis.nowell@gmail.com>
2025-01-16 16:30:42 +0000

    Added comments to the init file
    Added comments to the init files.

--- Database/src/__init__.py
+++ Database/src/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialise database
 db = SQLAlchemy()
 
 def create_app():
+    """Creates the database microservice's flask app with its configuration, initialises the database's tables,  
+    and links the app to the routes in the views file.   
+
+    Returns
+    -------
+    Flask
+        the database microservice's Flask app 
+    """
+
     app = Flask(__name__)   
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.db"
 
@@ -11,9 +21,11 @@ def create_app():
 
     from src import models
 
+    # Checks if an instance of the database already exists.
     with app.app_context():
         db.create_all()
 
+    # Point to the routes in the views file. 
     from src.views import main
     app.register_blueprint(main)
 
