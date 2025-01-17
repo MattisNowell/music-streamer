912a07d22c0b664645f790f2480719876f2f8a39
NowellM <mattis.nowell@gmail.com>
2025-01-17 15:32:45 +0000

    Changed backend and frontend to enable uploading music files.
    Changed the database and backend function to support audio file uploading. Changed the HTML and CSS to add a basic button for uploading the file. Added some rollback() functions when uploading and deleting from the database for safety measures.

--- Database/src/models.py
+++ Database/src/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy.orm import Mapped, mapped_column   
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import LargeBinary
 from src import db
 
 class Track(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=False)
-    artist: Mapped[str] = mapped_column(unique=False)
+    artist: Mapped[str] = mapped_column(unique=False) # To become an attribute linking to a user in the future user/account table 
+    data: Mapped[bytes] = mapped_column(LargeBinary)
